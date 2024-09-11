6fd4a9062d5483998a658e015abaeca6ef910598
Amrit Kumar <136947870+amritkum360@users.noreply.github.com>
2024-09-11 20:36:55 +0530

    Update const.js

--- src/utils/const.js
+++ src/utils/const.js
@@ -2,7 +2,10 @@
 // export const razorpayip = 'http://192.168.82.86:4000'
 
 export const backendip = 'https://zymplebackend.onrender.com'
-export const razorpayip = 'http://192.168.82.86:4000'
+// export const razorpayip = 'http://192.168.82.86:4000'
+export const razorpayip = 'https://rayzorpay-backend.onrender.com'
+
+
 
 
 
