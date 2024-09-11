874bf557fa9e7227e145074e8a6dd140aca28dee
Amrit Kumar <amritkum360@gmail.com>
2024-09-11 09:51:26 +0530

    url

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,4 +1,7 @@
-export const backendip = 'http://192.168.82.86:3003'
+// export const backendip = 'http://192.168.82.86:3003'
+// export const razorpayip = 'http://192.168.82.86:4000'
+
+export const backendip = 'https://zymplebackend.onrender.com'
 export const razorpayip = 'http://192.168.82.86:4000'
 
 
